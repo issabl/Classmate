e096d6bf396d1def3c2f35cc076a98068b511126
ary0711 <marypgoy@gmail.com>
2025-11-30 11:53:10 +0800

    updated forgot pass

--- backend/controllers/post.js
+++
@@ -1,6 +0,0 @@
-export const getPosts = (req, res) => {
-  res.json([
-    { id: 1, title: "First post" },
-    { id: 2, title: "Second post" }
-  ]);
-};
--- backend/routes/post.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getPosts } from "../controllers/post.js";
-
-const router = express.Router();
-
-// GET /api/posts
-router.get("/", getPosts);
-
-export default router;
